5879f41ffe9d87ef4c4943dca45c350360a9ad6e
lorber190103 <lorber190103@sr.htlweiz.at>
2024-04-03 14:48:01 +0200

    ValueDisplay Collapse

--- src/components/ValuesDisplay.vue
+++ src/components/ValuesDisplay.vue
@@ -6,6 +6,11 @@ export default {
   setup(props) {
     console.log(props.values)
   },
+  data() {
+        return {
+            editor_hidden: false
+        };
+    },
   emits: ['rename'],
   methods: {
     getTypeName(value: Value) {
@@ -45,33 +50,38 @@ export default {
       }
       return 'XXX'
     },
+    toggleEditor() {
+        this.editor_hidden = !this.editor_hidden;
+      }
+    },
   }
-}
 </script>
 
 <template>
-  <div class="row bg-primary mt-2 mb-1">
-    <div
-      class="col-1"
-      data-bs-toggle="tooltip"
-      data-bs-placement="top"
-      data-bs-title="Tooltip on top">
-      time
+  <div>
+    <!-- Labels row -->
+    <div class="row bg-primary mt-2 mb-1">
+      <div class="col-1" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top">
+        time
+      </div>
+      <div class="col-1">type</div>
+      <div class="col-2">value</div>
+      <div class="col-2">sensor</div>
+      <div class="col-2">location</div>
+      <span class="col text-end" v-on:click="toggleEditor()">
+        <i class="bi bi-caret-down-fill" title="Show ValueDisplay" v-if="editor_hidden"></i>
+        <i class="bi bi-caret-up-fill" title="Hide ValueDisplay" v-else></i>
+      </span>
     </div>
-    <div class="col-1">type</div>
-    <div class="col">value</div>
-    <div class="col-1">sensor</div>
-    <div class="col-1">location</div>
-  </div>
-  <div class="row bg-secondary rounded mt-1" v-for="value in values" :key="value">
-    <div class="col-1">
-      {{ value.time }}
+
+    <!-- Values rows -->
+    <div class="row bg-secondary rounded mt-1" v-for="value in values" :key="value" v-if="!editor_hidden">
+      <div class="col-1">{{ value.time }}</div>
+      <div class="col-1">{{ getTypeName(value) }}</div>
+      <div class="col-2">{{ value.value.toFixed(2) }} {{ getUnit(value) }}</div>
+      <div class="col-2">{{ getSensorName(value) }}</div>
+      <div class="col-2">{{ getLocationName(value) }}</div>
     </div>
-    <div class="col-1">
-      {{ getTypeName(value) }}
-    </div>
-    <div class="col">{{ value.value.toFixed(2) }} {{ getUnit(value) }}</div>
-    <div class="col-1">{{ getSensorName(value) }}</div>
-    <div class="col-1"> {{ getLocationName(value) }} </div>
   </div>
 </template>
+
